8f747b6139f210181ebb09b6ee529b6619c3a1bb
angel diaz <adiaz004@yahoo.com>
2015-07-26 15:00:18 -0700

    added Arrayd2D.swift

---
+++ Swiftris/Arrady2D.swift
@@ -0,0 +1,23 @@
+//
+//  Arrady2D.swift
+//  Swiftris
+//
+//  Created by angel diaz on 7/26/15.
+//  Copyright (c) 2015 Angel Diaz. All rights reserved.
+//
+
+import Foundation
+
+class Arrady2D<T> {
+    let columns: Int
+    let rows: Int
+    
+    var array: Array<T?>
+    
+    init(columns: Int, rows: Int) {
+        self.columns = columns
+        self.rows = rows
+        
+        array = Array<T?>(count:rows * columns, repeatedValue)
+    }
+}
--- Swiftris/GameViewController.swift
+++ Swiftris/GameViewController.swift
@@ -11,11 +11,22 @@ import SpriteKit
 
 class GameViewController: UIViewController {
 
+    var scene: GameScene!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        //configure the view.
+        let skView = view as! SKView
+        skView.multipleTouchEnabled = false
+    
+        // create and confiugre the scene.
+        scene = GameScene(size: skView.bounds.size)
+        scene.scaleMode = .AspectFill
+        
+        // present the scene.
+        skView.presentScene(scene)
     }
-
     override func prefersStatusBarHidden() -> Bool {
         return true
     }
